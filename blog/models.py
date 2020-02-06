82f6aa2662130569939f6e28ecff7da30bb6b330
hanminghe <hanminghe@shugia.com>
2020-02-06 11:00:04 +0800

    edit model for blog and makemigrations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+class BlogArticles(models.Model):
+    title=models.CharField(max_length=255)
+    author=models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    body=models.TextField()
+    publish=models.DateTimeField(timezone.now)
+
+    class Meta:
+        ordering=("-publish",)
+
+    def __str__(self):
+        return self.title
 
 # Create your models here.
