21fd949fff6697b56ccbd28ba477e48038b8bca5
hanminghe <hanminghe@shugia.com>
2020-02-06 11:09:18 +0800

    without default in publish field in the model, it did not show publish field in site

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ class BlogArticles(models.Model):
     title=models.CharField(max_length=255)
     author=models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     body=models.TextField()
-    publish=models.DateTimeField(timezone.now)
+    publish=models.DateTimeField(default=timezone.now)
 
     class Meta:
         ordering=("-publish",)
