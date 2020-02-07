d8064a1c209b2ab7fcb1355d605de3aa208e5734
hanminghe <hanminghe@shugia.com>
2020-02-07 19:58:56 +0800

    login form post part

--- account/views.py
+++ account/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from .form import LoginForm
+from django.http import HttpResponse
+from django.contrib.auth import authenticate,login
 # Create your views here.
-def login(request):
+def userlogin(request):
     if request.method=="GET":
         loginform=LoginForm()
         return render(request,"account/login.html",{"form":loginform})
+    elif request.method=="POST":
+        loginform=LoginForm(request.POST)
+        if loginform.is_valid():
+            cd=loginform.cleaned_data #without above line there was an error saying no attibute
+            user=authenticate(username=cd['username'],password=cd['password'])
+            if user:
+                login(request,user)
+                return HttpResponse("logged in")
+            else:
+                return HttpResponse("no authentication")
