4cc85e6cb91252aaa409d96023fe3a28b55aeada
hanminghe <hanminghe@shugia.com>
2020-02-11 21:08:35 +0800

    fix error on template name and redirect path

--- account/urls.py
+++ account/urls.py
@@ -9,10 +9,10 @@ urlpatterns = [
     path('register', views.userregister,name='register'),
     path('password-change', auth_views.PasswordChangeView.as_view(
         template_name="account/password_change_form.html",
-        success_url="/account/password-change-done.html"),
+        success_url="/account/password-change-done"),
         name='password-change'),
-    path('password-change-done', auth_views.PasswordChangeView.as_view(
-        template_name="account/password-change-done.html"),
+    path('password-change-done', auth_views.PasswordChangeDoneView.as_view(
+        template_name="account/password_change_done.html"),
         name='password-change-done'),
 
 ]
