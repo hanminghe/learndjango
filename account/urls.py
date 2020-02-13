245a34652627234b828e6a71d4dbfcef5db2266e
hanminghe <hanminghe@shugia.com>
2020-02-13 14:11:34 +0800

    reset password

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path ,reverse_lazy
 from . import views
 from django.contrib.auth import views as auth_views
 
@@ -9,10 +9,24 @@ urlpatterns = [
     path('register', views.userregister,name='register'),
     path('password-change', auth_views.PasswordChangeView.as_view(
         template_name="account/password_change_form.html",
-        success_url="/account/password-change-done"),
+        success_url=reverse_lazy('account:password-change-done')),
         name='password-change'),
     path('password-change-done', auth_views.PasswordChangeDoneView.as_view(
         template_name="account/password_change_done.html"),
         name='password-change-done'),
-
+    path('password-reset', auth_views.PasswordResetView.as_view(
+        template_name="account/password_reset_form.html",
+        email_template_name="account/password_reset_email.html",
+        success_url="/account/password-reset-done"),
+        name='password-reset'),
+    path('password-reset-done',auth_views.PasswordResetDoneView.as_view(
+        template_name="account/password_reset_done.html"),
+        name="password-reset-done"),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name="account/password_reset_confirm.html",
+        success_url="/account/password-reset-complete/"),
+        name='password-reset-confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name="account/password_reset_complete.html"),
+        name='password-reset-complete'),
 ]
