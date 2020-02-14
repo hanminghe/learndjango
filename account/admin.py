f12ad2e5d186b5e64b3b98f0979a6ae25a66937d
hanminghe <hanminghe@shugia.com>
2020-02-14 09:00:01 +0800

    regist userinfo to admin site

--- account/admin.py
+++ account/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile, UserInfo
 
 class UserProfileAdmin(admin.ModelAdmin):
     list_display=("user","birth","phone")
     list_filter=("phone",)
     search_fields=("phone",)
     ordering=['user']
+
+class UserInfoAdmin(admin.ModelAdmin):
+    list_display=("user","school","company","profession","address","aboutme")
+    list_filter=("school","company","profession")
+
 admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(UserInfo,UserInfoAdmin)
 # Register your models here.
