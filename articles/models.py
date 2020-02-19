81ad2c896a771ba685d12fd2cd57c4d718ef057c
hanminghe <hanminghe@shugia.com>
2020-02-19 14:03:50 +0800

    5 expand path in musite's url, 6 add url path in article

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import user
 
 # Create your models here.
+class AticleColumn(models.Model):
+    user=models.ForeignKey(User,on_delete=CASCADE,related_name='article_column')
+    column=models.CharField(max_length=200)
+    created=models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.column
