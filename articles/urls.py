57addd6e49e879f0697c5306b1b242c66da895db
hanminghe <hanminghe@shugia.com>
2020-02-20 21:58:03 +0800

    4 url

--- articles/urls.py
+++ articles/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('article-column',views.article_column,name='article-column'),
     path('edit-article-column',views.edit_article_column,name='edit-article-column'),
     path('del-article-column',views.del_article_column,name='del-article-column'),
+    path('article-post',views.article_post,name='article-post'),
 ]
